10c3ba7245943b02880ca9d9c8046665e5bbffc3
Hugh Perkins <hughperkins@gmail.com>
2017-08-12 19:58:04 +0100

    getdata.sh

---
+++ getdata.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function maybe_wget {
+    if [[ ! -f $2 ]]; then {
+        wget $1
+    } fi
+}
+
+mkdir data
+cd data
+maybe_wget http://people.csail.mit.edu/taolei/beer/annotations.json annotations.json
+maybe_wget http://people.csail.mit.edu/taolei/beer/review+wiki.filtered.200.txt.gz review+wiki.filtered.200.txt.gz
+
+for n in 1 2 3; do {
+    maybe_wget http://people.csail.mit.edu/taolei/beer/reviews.aspect${n}.heldout.txt.gz reviews.aspect${n}.heldout.txt.gz
+    maybe_wget http://people.csail.mit.edu/taolei/beer/reviews.aspect${n}.train.txt.gz reviews.aspect${n}.train.txt.gz
+} done
+
+maybe_wget http://people.csail.mit.edu/taolei/beer/select.py select.py
+
+for filename in $(ls *.gz); do {
+    gunzip -k ${filename}
+} done
+
+echo done
