218a0ff3f224804e4e2b2611e40821d8e5dc7f3d
Hugh Perkins <hughperkins@gmail.com>
2017-08-13 16:01:19 +0100

    start to write Generator, and move some utilities bits into embeddings_helper.py

---
+++ embeddings_helper.py
@@ -0,0 +1,52 @@
+import torch
+import pickle
+
+
+def combine_embeddings(embeddings_list, idx_by_word_list):
+    """
+    any duplicates are assumed to be identical, ie if a word
+    exists in both embeddings, the embedding should already be identical,
+    otherwise behavior is undefined
+    """
+    num_lists = len(embeddings_list)
+    idx_by_word_new = {}
+    vals = []
+    words = []
+    for i in range(num_lists):
+        idx_by_word = idx_by_word_list[i]
+        embedding = embeddings_list[i]
+        for word, idx in idx_by_word.items():
+            if word not in idx_by_word_new:
+                new_idx = len(idx_by_word_new)
+                idx_by_word_new[word] = new_idx
+                vals.append(embedding[idx])
+                words.append(word)
+    new_V = len(idx_by_word_new)
+    num_hidden = vals[0].shape[-1]
+    print('num_hidden', num_hidden)
+    new_embedding = torch.zeros(new_V, num_hidden)
+    for v in range(new_V):
+        new_embedding[v] = vals[v]
+    return {
+        'embedding': new_embedding,
+        'idx_by_word': idx_by_word_new,
+        'num_hidden': num_hidden,
+        'words': words
+    }
+
+
+def load_embedded_data(in_filename, max_examples, aspect_idx):
+    print('loading %s ...' % in_filename)
+    with open(in_filename, 'rb') as f:
+        d = pickle.load(f)
+    print(' ... loaded')
+    if max_examples > 0:
+        for k in ['x_idxes', 'x', 'y']:
+            d[k] = d[k][:max_examples]
+    d['N'] = len(d['x'])
+    N = d['N']
+    d['y_aspect'] = torch.zeros(N)
+    for n, yv in enumerate(d['y']):
+        d['y_aspect'][n] = yv[aspect_idx].item()
+    d['num_hidden'] = d['embedding'].shape[1]
+    return d
--- train.py
+++ train.py
@@ -4,11 +4,14 @@ that against the test data first
 """
 
 import torch
-import numpy as np
 import argparse
-import pickle
-import myio
 from torch import nn, optim, autograd
+import myio
+import embeddings_helper
+
+
+def rand_uniform(shape, min_value, max_value):
+    return torch.rand(shape) * (max_value - min_value) + min_value
 
 
 class Encoder(nn.Module):
@@ -62,72 +65,84 @@ class Encoder(nn.Module):
         return x
 
 
-def combine_embeddings(embeddings_list, idx_by_word_list):
+class Generator(nn.Module):
     """
-    any duplicates are assumed to be identical, ie if a word
-    exists in both embeddings, the embedding should already be identical,
-    otherwise behavior is undefined
+    This will be the 'independent' form for now
+
+    It's basically almost identical to the Encoder, except:
+    - bidirectional LSTM, not unidirectional
+    - the linear runs at each word position, so there are:
+    - ... outputs given for each position
+
+    Some things taht are the same then:
+    - embeddings
+    - number of layers
+    - fact that it contains an rnn, and the type of rnn (RCNN in the paper,
+      LSTM here)
+    - the fact taht we have in sequence:
+        - embedding, then
+        - lstm/rnn, then
+        - linear
     """
-    num_lists = len(embeddings_list)
-    idx_by_word_new = {}
-    vals = []
-    words = []
-    for i in range(num_lists):
-        idx_by_word = idx_by_word_list[i]
-        embedding = embeddings_list[i]
-        for word, idx in idx_by_word.items():
-            if word not in idx_by_word_new:
-                new_idx = len(idx_by_word_new)
-                idx_by_word_new[word] = new_idx
-                vals.append(embedding[idx])
-                words.append(word)
-    new_V = len(idx_by_word_new)
-    num_hidden = vals[0].shape[-1]
-    print('num_hidden', num_hidden)
-    new_embedding = torch.zeros(new_V, num_hidden)
-    for v in range(new_V):
-        new_embedding[v] = vals[v]
-    return {
-        'embedding': new_embedding,
-        'idx_by_word': idx_by_word_new,
-        'num_hidden': num_hidden,
-        'words': words
-    }
+    def __init__(self, embeddings, num_layers):
+        super().__init__()
+        self.num_hidden = embeddings.shape[1]
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(
+            embeddings.shape[0],
+            self.num_hidden
+        )
+        self.embedding.weight.data = embeddings
+        self.embedding.weight.requires_grad = False
+        self.lstm = nn.LSTM(
+            input_size=self.num_hidden,
+            hidden_size=self.num_hidden,
+            num_layers=num_layers,
+            bidirectional=True)
+        self.initial_state = None
+        self.initial_cell = None
+        self.linear = nn.Linear(self.num_hidden, 1)
 
+    def forward(self, x):
+        """
+        x should be [seq_len][batch_size]
+        """
+        batch_size = x.size()[1]
+        # we reuse initial_state and initial_cell, if they havent changed
+        # since last time.
+        if self.initial_state is None or self.initial_state.size()[1] != batch_size:
+            self.initial_state = autograd.Variable(torch.zeros(
+                self.num_layers * 2,
+                batch_size,
+                self.num_hidden
+            ))
+            self.initial_cell = autograd.Variable(torch.zeros(
+                self.num_layers * 2,
+                batch_size,
+                self.num_hidden
+            ))
+        x = self.embedding(x)
+        x, _ = self.lstm(x, (self.initial_state, self.initial_cell))
+        # x = x[:, -1, :]
+        print('after lstm: x.data.shape', x.data.shape)
+        x = self.linear(x)
+        print('     after linear: x.data.shape', x.data.shape)
 
-def rand_uniform(shape, min_value, max_value):
-    return torch.rand(shape) * (max_value - min_value) + min_value
-
-
-def load_embedded_data(in_filename, max_examples, aspect_idx):
-    print('loading %s ...' % in_filename)
-    with open(in_filename, 'rb') as f:
-        d = pickle.load(f)
-    print(' ... loaded')
-    if max_examples > 0:
-        for k in ['x_idxes', 'x', 'y']:
-            d[k] = d[k][:max_examples]
-    d['N'] = len(d['x'])
-    N = d['N']
-    d['y_aspect'] = torch.zeros(N)
-    for n, yv in enumerate(d['y']):
-        d['y_aspect'][n] = yv[aspect_idx].item()
-    d['num_hidden'] = d['embedding'].shape[1]
-    return d
+        return x
 
 
 def run(
         in_train_file_embedded, aspect_idx, max_train_examples, batch_size, learning_rate,
         in_validate_file_embedded, max_validate_examples, validate_every):
-    train_d = load_embedded_data(
+    train_d = embeddings_helper.load_embedded_data(
         in_filename=in_train_file_embedded,
         max_examples=max_train_examples,
         aspect_idx=aspect_idx)
-    validate_d = load_embedded_data(
+    validate_d = embeddings_helper.load_embedded_data(
         in_filename=in_validate_file_embedded,
         max_examples=max_validate_examples,
         aspect_idx=aspect_idx)
-    combined = combine_embeddings(
+    combined = embeddings_helper.combine_embeddings(
         embeddings_list=[train_d['embedding'], validate_d['embedding']],
         idx_by_word_list=[train_d['idx_by_word'], validate_d['idx_by_word']])
     embedding = combined['embedding']
