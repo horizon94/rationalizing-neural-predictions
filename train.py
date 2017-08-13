f6356ab993568484c8e50d99733edc47684f42a9
Hugh Perkins <hughperkins@gmail.com>
2017-08-13 14:55:34 +0100

    add validate, and validate loss goes down too

--- train.py
+++ train.py
@@ -62,52 +62,128 @@ class Encoder(nn.Module):
         return x
 
 
+def combine_embeddings(embeddings_list, idx_by_word_list):
+    """
+    any duplicates are assumed to be identical, ie if a word
+    exists in both embeddings, the embedding should already be identical,
+    otherwise behavior is undefined
+    """
+    num_lists = len(embeddings_list)
+    # unk_idx = idx_by_word_list[0]['<unk>']
+    # pad_idx = idx_by_word_list[0]['<pad>']
+    idx_by_word_new = {}
+    vals = []
+    words = []
+    for i in range(num_lists):
+        idx_by_word = idx_by_word_list[i]
+        embedding = embeddings_list[i]
+        for word, idx in idx_by_word.items():
+            if word not in idx_by_word_new:
+                new_idx = len(idx_by_word_new)
+                idx_by_word_new[word] = new_idx
+                vals.append(embedding[idx])
+                words.append(word)
+    new_V = len(idx_by_word_new)
+    num_hidden = vals[0].shape[-1]
+    print('num_hidden', num_hidden)
+    new_embedding = torch.zeros(new_V, num_hidden)
+    for v in range(new_V):
+        new_embedding[v] = vals[v]
+    return {
+        'embedding': new_embedding,
+        'idx_by_word': idx_by_word_new,
+        'num_hidden': num_hidden,
+        'words': words
+    }
+    # return new_embedding
+
+        # assert idx_by_word['<unk>'] == unk_idx
+        # assert pad_idx == idx_by_word['<pad>']
+
+
 def rand_uniform(shape, min_value, max_value):
     return torch.rand(shape) * (max_value - min_value) + min_value
 
 
-def run(in_train_file_embedded, aspect_idx, max_train_examples, batch_size, learning_rate):
-    print('loading training data...')
-    with open(in_train_file_embedded, 'rb') as f:
+def load_embedded_data(in_filename, max_examples, aspect_idx):
+    print('loading %s ...' % in_filename)
+    with open(in_filename, 'rb') as f:
         d = pickle.load(f)
     print(' ... loaded')
-    # reminder, d is: embedding, idx_by_word, words, x, y, x_idxes
-    embedding = d['embedding']
-    y = d['y']
-    # print('y.shape', y.shape)
-    # print('y[0]', y[0])
-    x_idxes = d['x_idxes']
-    x = d['x']
-    idx_by_word = d['idx_by_word']
-    if max_train_examples > 0:
-        x_idxes = x_idxes[:max_train_examples]
-        y = y[:max_train_examples]
-        x = x[:max_train_examples]
-    N = len(x_idxes)
-    y_aspect = torch.zeros(N)
-    for n, yv in enumerate(y):
-        y_aspect[n] = yv[aspect_idx].item()
-    print('y_aspect.shape', y_aspect.shape)
-    print('y_aspect[:5]', y_aspect[:5])
-    print('num training examples', len(x_idxes))
-    # handle unk
-    unk_idx = idx_by_word['<unk>']
-    num_hidden = embedding.shape[1]
+    if max_examples > 0:
+        for k in ['x_idxes', 'x', 'y']:
+            d[k] = d[k][:max_examples]
+    d['N'] = len(d['x'])
+    N = d['N']
+    d['y_aspect'] = torch.zeros(N)
+    for n, yv in enumerate(d['y']):
+        d['y_aspect'][n] = yv[aspect_idx].item()
+    # d['unk_idx'] = d['idx_by_word']['<unk>']
+    # d['pad_idx'] = d['idx_by_word']['<pad>']
+    d['num_hidden'] = d['embedding'].shape[1]
+    return d
+
+
+def run(
+        in_train_file_embedded, aspect_idx, max_train_examples, batch_size, learning_rate,
+        in_validate_file_embedded, max_validate_examples, validate_every):
+    # print('loading training data...')
+    # with open(in_train_file_embedded, 'rb') as f:
+    #     d = pickle.load(f)
+    # print(' ... loaded')
+    # # reminder, d is: embedding, idx_by_word, words, x, y, x_idxes
+    # embedding = d['embedding']
+    # y = d['y']
+    # # print('y.shape', y.shape)
+    # # print('y[0]', y[0])
+    # x_idxes = d['x_idxes']
+    # x = d['x']
+    # idx_by_word = d['idx_by_word']
+    # if max_train_examples > 0:
+    #     x_idxes = x_idxes[:max_train_examples]
+    #     y = y[:max_train_examples]
+    #     x = x[:max_train_examples]
+    # N = len(x_idxes)
+    # y_aspect = torch.zeros(N)
+    # for n, yv in enumerate(y):
+    #     y_aspect[n] = yv[aspect_idx].item()
+    # print('y_aspect.shape', y_aspect.shape)
+    # print('y_aspect[:5]', y_aspect[:5])
+    # print('num training examples', len(x_idxes))
+    # # handle unk
+    # unk_idx = idx_by_word['<unk>']
+    # num_hidden = embedding.shape[1]
+    train_d = load_embedded_data(
+        in_filename=in_train_file_embedded,
+        max_examples=max_train_examples,
+        aspect_idx=aspect_idx)
+    validate_d = load_embedded_data(
+        in_filename=in_validate_file_embedded,
+        max_examples=max_validate_examples,
+        aspect_idx=aspect_idx)
+    combined = combine_embeddings(
+        embeddings_list=[train_d['embedding'], validate_d['embedding']],
+        idx_by_word_list=[train_d['idx_by_word'], validate_d['idx_by_word']])
+    embedding = combined['embedding']
+    num_hidden = combined['num_hidden']
+    idx_by_word = combined['idx_by_word']
+    words = combined['words']
 
     # these numbers, ie -0.05 to 0.05 come from
     # https://github.com/taolei87/rcnn/blob/master/code/nn/initialization.py#L79
+    # embedding_train = train_d['embedding']
+    unk_idx = idx_by_word['<unk>']
+    pad_idx = idx_by_word['<pad>']
+    # assert validate_d['unk_idx'] == unk_idx
+    # num_hidden = combine['num_hidden']
     embedding[unk_idx] = rand_uniform((num_hidden,), -0.05, 0.05)
     model = Encoder(embeddings=embedding, num_layers=2)
-    pad_idx = idx_by_word['<pad>']
-    # print('len(batches_x)', len(batches_x))
-    # print('len(batches_y)', len(batches_y))
-    # print('batches_x[0][:3]', batches_x[0][:3])
-    # print('batches_y[0][:3]', batches_y[0][:3])
     params = filter(lambda p: p.requires_grad, model.parameters())
     opt = optim.Adam(params=params, lr=learning_rate)
     epoch = 0
     while True:
-        batches_x, batches_y = myio.create_batches(x=x_idxes, y=y_aspect, batch_size=batch_size, padding_id=pad_idx)
+        batches_x, batches_y = myio.create_batches(
+            x=train_d['x_idxes'], y=train_d['y_aspect'], batch_size=batch_size, padding_id=pad_idx)
         num_batches = len(batches_x)
         epoch_loss = 0
         for b in range(num_batches):
@@ -123,16 +199,36 @@ def run(in_train_file_embedded, aspect_idx, max_train_examples, batch_size, lear
             opt.step()
             # print('loss %.3f' % loss)
         print('epoch %s loss %.3f' % (epoch, epoch_loss))
+
+        def run_validation():
+            batches_x, batches_y = myio.create_batches(
+                x=validate_d['x_idxes'], y=validate_d['y_aspect'], batch_size=batch_size, padding_id=pad_idx)
+            num_batches = len(batches_x)
+            epoch_loss = 0
+            for b in range(num_batches):
+                print('b %s' % b)
+                bx = autograd.Variable(batches_x[b])
+                by = autograd.Variable(batches_y[b])
+                out = model.forward(bx)
+                loss = ((by - out) * (by - out)).sum().sqrt()
+                epoch_loss += loss.data[0]
+            print('validate epoch %s loss %.3f' % (epoch, epoch_loss))
+
+        if (epoch + 1) % validate_every == 0:
+            run_validation()
         epoch += 1
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--in-train-file-embedded', type=str, default='data/reviews.aspect1.train.emb')
+    parser.add_argument('--in-validate-file-embedded', type=str, default='data/reviews.aspect1.heldout.emb')
     parser.add_argument('--aspect-idx', type=int, default=0)
     parser.add_argument('--batch-size', type=int, default=256)
     parser.add_argument('--learning-rate', type=float, default=0.001)
     parser.add_argument('--max-train-examples', type=int, default=0, help='0 means all')
+    parser.add_argument('--max-validate-examples', type=int, default=0, help='0 means all')
+    parser.add_argument('--validate-every', type=int, default=1, help='after how many epochs run validation')
     parser.add_argument
     args = parser.parse_args()
     run(**args.__dict__)
